dbd8d101a0d675b8629278860a5a230c97d7638d
«Helen <helen-piter@mail.ru>
2022-10-05 04:45:13 +0300

    Ввела исходный массив символов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача : Напишите программу, которая из имеющегося массива
+// строк формирует массив из строк, длина которых равна меньше или равна три символа
+//Перевоначальный массив можно ввести с клавиатуры либо задать
+// на старте выполнения алгоритма
+
+using static System.Console;
+Clear();
+
+string[] arr =
+
+{
+    "hello", "2", "world", ":-)",
+    "Russia", "Denmark", "-2",  "1234", "1567",
+    "computer  science",
+};
+
